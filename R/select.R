4c62fce13f8e132df880941c41874b0cf6fdc7e1
THybel <au564420@uni.au.dk>
2024-08-22 10:48:53 +0200

    init pkg with fun select2

---
+++ R/select.R
@@ -0,0 +1,20 @@
+
+
+#' Subset variables from data frame
+#'
+#' @param df A data frame
+#' @param vars A vector of either characters (variables names), of integers (variable indices), or of booleans.
+#'
+#' @return A data frame with the subsetted variables.
+#' @export
+#'
+#' @examples
+#' select2(iris, c("Sepal.Length", "Species"))
+#' select2(iris, 1:2)
+#' select2(iris, c(TRUE, TRUE, FALSE, FALSE, FALSE))
+select2 <- function(df, vars) {
+  df[ , vars]
+}
+
+
+
